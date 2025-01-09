d8868a83fce0da01698f33ddf4bb870e1ca9dcfc
Priscilla Barbosa <priscillabarbosa@outlook.com>
2025-01-08 22:09:18 -0300

    exercícios default

---
+++ exercicios-basicos/parametro-default/index.php
@@ -0,0 +1,18 @@
+<?php
+
+  function teste($a = "teste") {
+
+    echo "O valor de A é: $a <br>";
+
+  }
+
+  teste();
+  teste("P");
+
+  function testando($b, $a = "x") {
+
+    echo "O valor de a é: $a e o valor de b é: $b <br>";
+
+  }
+
+  testando("1");
