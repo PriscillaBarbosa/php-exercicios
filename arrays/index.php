fab3bce1698e57dc6f69c4f8b7c5dfc11113293e
Priscilla Barbosa <priscillabarbosa@outlook.com>
2025-01-17 23:59:08 -0300

    exercícios array

--- arrays/index.php
+++ arrays/index.php
@@ -1,6 +1,33 @@
 <?php
 
+//adicionando valores
+
 $arr = [];
+
+print_r($arr);
+echo "<br>";
+
+$arr[0] = 10;
+
+print_r($arr);
+echo "<br>";
+
+$arr[1] = 15;
+
+print_r($arr);
+echo "<br>";
+
+$arr[5] = 25;
+print_r($arr);
+echo "<br>";
+
+//modificando valores
+$arr[1] += 55;
+print_r($arr);
+echo "<br>";
+
+$arrAssoc = [];
+$arrAssoc["carro"] = "BMW";
 
 print_r($arr);
 echo "<br>";
